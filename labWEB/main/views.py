2dc8cc86256559f0e438ee0ea47b4bce43f240a4
lab1_web <goodpromised41@gmail.com>
2024-04-11 21:22:41 +0300

    --

--- labWEB/main/views.py
+++ labWEB/main/views.py
@@ -20,76 +20,19 @@ def index(request):
 
 
 def registration(request):
-    error = ''
     if request.method == 'POST':
-        # Получение данных из POST запроса (переданных через AJAX)
-        data = json.loads(request.body.decode('utf-8'))
-
-        first_name = data.get('firstName', '')
-        last_name = data.get('lastName', '')
-        email = data.get('email', '')
-        username = data.get('username', '')
-        password = data.get('password', '')
-        confirm_password = data.get('confirmPassword', '')
-        agree_terms = data.get('agreeTerms', False)
-        age = data.get('age', '')
-        gender = data.get('gender', '')
-
-        # Проверка данных
-        errors = []
-
-        if not (2 <= len(first_name) <= 15) or not first_name.isalpha():
-            errors.append("Недопустимое имя")
-
-        if not (2 <= len(last_name) <= 15) or not last_name.isalpha():
-            errors.append("Недопустимая фамилия")
-
-        if not email:
-            errors.append("Email не указан")
-        elif not email_validator(email):
-            errors.append("Недопустимый email")
-
-        if len(username) < 6:
-            errors.append("Логин должен содержать не менее 6 символов")
-
-        if len(password) < 8:
-            errors.append("Пароль должен содержать не менее 8 символов")
-        elif not any(c.isupper() for c in password) or not any(c.islower() for c in password) \
-                or not any(c.isdigit() for c in password) or not any(c.isalnum() for c in password):
-            errors.append("Пароль должен включать хотя бы одну заглавную букву, одну строчную букву, одну цифру и один специальный символ")
-
-        if password != confirm_password:
-            errors.append("Пароли не совпадают")
-
-        if not agree_terms:
-            errors.append("Вы должны принять правила")
-
-        if errors:
-            return JsonResponse({'errors': errors}, status=400)
-
-        # Хеширование пароля
-        hashed_password = make_password(password)
-
-        # Сохранение пользователя
-        try:
-            user = Register.objects.create(
-                name=first_name,
-                surname=last_name,
-                email=email,
-                login=username,
-                password=hashed_password,
-                gender=gender
-            )
-            user.save()
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            login = form.cleaned_data['login']
+            if Register.objects.filter(login=login).exists():
+                return JsonResponse({'errors': {'login': [{'message': 'Регистрация с таким Логин уже существует.', 'code': 'unique'}]}}, status=400)
+            form.save()
             return JsonResponse({'success': 'Регистрация успешно завершена'})
-        except Exception as e:
-            return JsonResponse({'errors': 'Ошибка при регистрации пользователя: ' + str(e)}, status=500)
-
-    elif request.method == 'POST':
-        return JsonResponse({'errors': 'Метод запроса должен быть POST ' + request.method}, status=405)
+        else:
+            errors = form.errors.as_json()
+            return JsonResponse({'errors': errors}, status=400)
     else:
-        return HttpResponseNotAllowed(['GET', 'POST'])
-
+        return JsonResponse({'errors': 'Метод запроса должен быть POST'}, status=405)
 def email_validator(email):
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     if re.match(pattern, email):
